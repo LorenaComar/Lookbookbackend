c66a0e78e32b1c41eb19402763d61383fc40e8f7
LorenaComar <114241070+LorenaComar@users.noreply.github.com>
2022-11-03 11:04:20 -0300

    backend

--- app.js
+++ app.js
@@ -2,6 +2,30 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const mysql = require('mysql2');
+
+const con = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
+});
+
+con.connect((err) => {
+    if(err){
+        console.log('Erro conectando ao banco de dados...', err)
+        return
+    }
+    console.log('Conexão estabelecida!')
+})
+
+con.end((err) => {
+    if(err){
+        console.log('Erro to finish connection...', err)
+        return
+    }
+    console.log('Conexão finalizada!')
+})
 
 app.use(bodyParser.json());
 
